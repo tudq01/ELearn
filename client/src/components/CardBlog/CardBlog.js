3dd06f98a4b420217ffe566cbfe838c4eb9d6ec3
TuDang1 <dangquoctuhn@gmail.com>
2022-05-28 08:42:13 +0700

    Update CardBlog.js

--- client/src/components/CardBlog/CardBlog.js
+++ client/src/components/CardBlog/CardBlog.js
@@ -1,30 +1,35 @@
-import React from 'react'
-import "./CardBlog.css"
+import React from "react";
+import "./CardBlog.css";
 import { Link } from "react-router-dom";
+import TokenService from "../../service/tokenService";
 function CardBlog(props) {
-
-  
-
   const handleClick = () => {
-    console.log("hello");
-  }
+    if (!TokenService.getuserInfo()) {
+      alert("You need to Login");
+    }
+  };
   return (
     <>
       <div className="blog-item">
-        <h1>{props.name}  TEST  {props.test}</h1>
-        <span>{props.time} phut  </span> <br></br>
-        <span>{props.part} phan thi |  {props.numberQuestion} cau hoi  </span>
-        <Link to=    {`/test/${props._id}`}
-        
-     
-            state={props}
-        >
-          <button onClick={handleClick}>Xem ket qua</button> 
-        </Link>
-
+        <h1>
+          {props.name} TEST {props.test}
+        </h1>
+        <span>{props.time} phut </span> <br></br>
+        <span>
+          {props.part} phan thi | {props.numberQuestion} cau hoi{" "}
+        </span>
+        {TokenService.getuserInfo() ? (
+          <Link to={`/test/${props._id}`} state={props}>
+            <button onClick={handleClick}>Xem ket qua</button>
+          </Link>
+        ) : (
+          <Link to={`/login`} state={props}>
+            <button onClick={handleClick}>Xem ket qua</button>
+          </Link>
+        )}
       </div>
     </>
-  )
+  );
 }
 
-export default CardBlog
+export default CardBlog;
