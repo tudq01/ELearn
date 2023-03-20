5ebf9256941ee2d2f32e76c9916768fcfa932cc3
TuDang1 <dangquoctuhn@gmail.com>
2023-03-20 13:44:22 +0700

    Update backend import

--- backend/index.js
+++ backend/index.js
@@ -52,7 +52,7 @@ app.use(
 );  */
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://elearn-web.onrender.com",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
